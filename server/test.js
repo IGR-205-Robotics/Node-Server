be7c6d90c36c8bde1342cebb5f767f6be41acb67
Bex <rebeccaruva@newschool.edu>
2022-06-26 12:52:39 +0200

    updated server to send commands to esp server

--- server/test.js
+++ server/test.js
@@ -1,7 +1,29 @@
 const { Marvelmind } = require('./marvelmind');
+const http = require('http');
 const dgram = require('dgram');
 const { send } = require('process');
-const server = dgram.createSocket('udp4');
+const socket_udp = dgram.createSocket('udp4');
+const fetch = require('node-fetch');
+
+// let server_host = "0.0.0.0";//"192.168.0.100";
+// let server_port = 80;
+let socket_host = "192.168.0.1";
+let socket_port = 23;
+
+// const server = http.createServer(function (req, res) {
+//     if(req.url == "/quit") {
+//         res.writeHead(200,  {'Content-Type':'text/html'});
+//         res.end('<h1>...quitting</h1>');
+//         console.log('i show quit');
+//     } else {
+//         res.writeHead(200,  {'Content-Type':'text/html'});
+//         res.end('<h1>Hello World!</h1>');
+//         console.log('i welcome');
+//     }
+// });
+// server.listen(server_port, server_host, () => {
+//     console.log(`Server is running on http://${server_host}:${server_port}`);
+// });
 
 global.g_command = "";
 global.g_address = 0;
@@ -28,38 +50,35 @@ marvelmind.on('hedgehogMilimeter', (hedgehogAddress, hedgehogCoordinates) => {
 // marvelmind.on('telemetry', (deviceAddress, telemetryData) => {
 //   console.log('telemetry', deviceAddress, telemetryData);
 // });
-let ethernet_ip = "192.168.0.1";
-let ethernet_port = 23;
-let multicast_address = "255.255.255.0";
 
-server.bind(5500, function() {
-    // server.addMembership(multicast_address, ethernet_ip);
-    // console.log("listening on a specific address: " + ethernet_ip);
-  });
 
-server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
+socket_udp.bind(5500);
+
+socket_udp.on('error', (err) => {
+    console.log(`socket_udp error:\n${err.stack}`);
+    socket_udp.close();
 });
 
-server.on('message', (msg, senderInfo) => {
+socket_udp.on('message', (msg, senderInfo) => {
     console.log('Messages received '+ msg)
     if (msg == "UPDATE") {
         data = getUpdatedCoords();
-        server.send(JSON.stringify(data),senderInfo.port,senderInfo.address,function(error){
+        socket_udp.send(JSON.stringify(data),senderInfo.port,senderInfo.address,function(error){
             if(error){
             client.close();
             }else{
             // console.log('positions sent');
         }
         });
-    } else if (msg == "QUIT") {
-        process.exit(0);
+    } else if (msg == "esp") {
+        //     process.exit(0);
     } else {
         g_command = msg;
-        sendCommand();
+        sendESPMessage(msg).then(data => {
+            console.log(data);
+        });
         //send to unity
-        server.send("saved command :" + msg,senderInfo.port,senderInfo.address,function(error){
+        socket_udp.send("saved command :" + msg,senderInfo.port,senderInfo.address,function(error){
             if(error){
               client.close();
             }else{
@@ -67,7 +86,7 @@ server.on('message', (msg, senderInfo) => {
             }
         });
         //send to access point
-        server.send(msg,ethernet_port,ethernet_ip,function(error){
+        socket_udp.send(msg,socket_port,socket_host,function(error){
             if(error){
                 client.close();
             }else{
@@ -77,9 +96,9 @@ server.on('message', (msg, senderInfo) => {
     }
 });
 
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening on ${address.address}:${address.port}`);
+socket_udp.on('listening', () => {
+    const address = socket_udp.address();
+    console.log(`socket_udp listening on ${address.address}:${address.port}`);
 });
 
 function getUpdatedCoords() {
@@ -91,6 +110,9 @@ function setUpdatedCoords(id, coords) {
     g_coords = coords;
 }
 
-function sendCommand() {
-    //send command to robo saved in g_command
+async function sendESPMessage(comm) {
+    const response = await fetch(`http://192.168.0.105:80/message?command=${comm}`);
+    const data = await response.text();
+    console.log('send message to esp');
+    return data;
 }
